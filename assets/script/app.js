1375bec5d6a6219aa3b62bbd370bbef7f8e46d5c
babboe1 <ayodelaustinab@gmail.com>
2022-07-26 12:02:23 +0100

    feat: create const DOM variable

--- assets/script/app.js
+++ assets/script/app.js
@@ -2,4 +2,5 @@ const HEAD_TAG = document.getElementById('head');
 const BACKDROP = document.getElementById('backdrop');
 const MENU_BTN = document.querySelector('.menu-btn');
 const MENU_MODAL = document.getElementById('menu-modal');
+const IMAGE_ID = document.querySelector('.menu-pic');
 
