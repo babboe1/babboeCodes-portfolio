651e9b8f4fc13766bbdeff14054bec7b5e543672
babboe1 <ayodelaustinab@gmail.com>
2022-07-26 12:02:34 +0100

    feat: create const DOM variable

--- assets/script/app.js
+++ assets/script/app.js
@@ -3,4 +3,5 @@ const BACKDROP = document.getElementById('backdrop');
 const MENU_BTN = document.querySelector('.menu-btn');
 const MENU_MODAL = document.getElementById('menu-modal');
 const IMAGE_ID = document.querySelector('.menu-pic');
+const LINE = document.querySelector('.nav-bar__list--after');
 
