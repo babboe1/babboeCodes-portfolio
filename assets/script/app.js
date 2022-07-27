f507df979f2c6422b81843c53f406a7d0e0e62d1
babboe1 <ayodelaustinab@gmail.com>
2022-07-27 17:22:42 +0100

    feat: create const DOM variable

--- assets/script/app.js
+++ assets/script/app.js
@@ -33,4 +33,6 @@ const MESSAGE_SECTION_CONTENT = document.querySelector(
 const CARTOON_PIC2 = document.querySelector('.cartoon-pic2');
 const MINI_FORM = document.querySelector('.message-section--form');
 const CONTACT_PAGE_FORM = document.querySelector('.contact-form-action');
+const GRATITUDE = document.querySelector('.gratitude');
+const GRATITUDE_BTN = document.querySelector('.gratitude__button');
 
