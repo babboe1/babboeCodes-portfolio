5d39f0be8d88c957b9c33f2cd071c17cbd08907c
babboe1 <ayodelaustinab@gmail.com>
2022-07-29 18:41:04 +0100

    feat: create event listerner

--- assets/script/app.js
+++ assets/script/app.js
@@ -302,4 +302,5 @@ const toggleModeHandler = () => {
 
 BACKDROP.addEventListener('click', backDropHandler);
 MENU_BTN.addEventListener('click', menuBtnHandler);
+IMAGE_ID.addEventListener('click', menuBtnHandler);
 
