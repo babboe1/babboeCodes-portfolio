d0c895f9f237d0d261489092a088e8b87357a5d0
babboeCodes <babboecodes@gmail.com>
2023-03-27 10:39:25 +0100

    chore : add modal component to layout

--- src/components/Layouts/Layouts.js
+++ src/components/Layouts/Layouts.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
+import Modal from '../Modals/Modal/Modal';
+import Gratitude from './Footer/Gratitude/Gratitude';
 
 const Layouts = ({ children }) => {
    return (
       <>
          <Header />
-         <main id="main">{children}</main>
+         <main id="main">
+            {children}
+            <Modal>
+               <Gratitude />
+            </Modal>
+         </main>
          <Footer />
       </>
    );
